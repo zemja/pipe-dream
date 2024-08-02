4cf75d706172b01ef6f338b19230432ab0ed2829
zemja <mail@zemja.org>
2024-08-02 17:40:01 +0100

    Basic distinguishing between value types

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
 fn main() {
+    println!("cargo::rerun-if-changed=ui/main.slint");
     slint_build::compile("ui/main.slint").unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,57 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     let weak = mw.as_weak();
     mw.on_accepted(move |text| {
         let Some(mw) = weak.upgrade() else { return };
+
+        match nu.evaluate(&text).map(output::Output::from) {
+            Ok(output::Output::Empty) => mw.set_type(Type::Empty),
+
+            Ok(output::Output::Value(value)) => {
+                mw.set_type(Type::Value);
+                mw.set_value(format!("{value:?}").into());
+            }
+
+            Ok(output::Output::List(list)) => {
+                let list: Vec<_> = list.into_iter()
+                    .map(|value| format!("{value:?}").into())
+                    .collect();
+                mw.set_type(Type::List);
+                mw.set_list(slint::ModelRc::new(slint::VecModel::from(list)));
+            }
+
+            Ok(output::Output::Table(table)) => {
+                let rows = table.rows.into_iter()
+                    .map(|row| row.into_iter().map(|row| format!("{row:?}")).collect());
+                let table: Vec<Vec<String>> = [table.header_row].into_iter().chain(rows).collect();
+                let rows: Vec<slint::ModelRc<slint::SharedString>> = table.into_iter()
+                    .map(|row| {
+                        let row: Vec<slint::SharedString> = row.into_iter()
+                            .map(slint::SharedString::from)
+                            .collect();
+                        slint::ModelRc::new(slint::VecModel::from(row))
+                    })
+                    .collect();
+                let table = slint::ModelRc::new(slint::VecModel::from(rows));
+                mw.set_type(Type::Table);
+                mw.set_table(table);
+            }
+
+            Ok(output::Output::Raw(Ok(raw))) => {
+                mw.set_type(Type::Raw);
+                mw.set_raw(slint::SharedString::from(String::from_utf8_lossy(&raw).to_string()));
+            }
+
+            Ok(output::Output::Raw(Err(err))) => {
+                mw.set_type(Type::Error);
+                mw.set_error(err.to_string().into());
+            }
+
+            Err(err) => {
+                mw.set_type(Type::Error);
+                mw.set_error(err.to_string().into());
+            }
+        }
+
         mw.set_prompt("".into());
-        mw.set_text(format!("{:#?}", nu.evaluate(&text).map(output::Output::from)).into());
     });
 
     mw.run()?;
